e407e214dc4474fc0ad6af0a1e272e0bca0f3e46
Mark Allibone <m.allibone@rey-automation.ch>
2020-01-12 15:40:26 +0100

    Add project files.

---
+++ AddressEntry/AddressEntry/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using AddressEntry.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AddressEntry
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ AddressEntry/AddressEntry/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace AddressEntry.ViewModels
+{
+    internal class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+            HandleOpenMap = ReactiveCommand.Create(() => IsPickingLocationFromMap = true);
+        }
+
+        [Reactive] public string Street { get; set; }
+        [Reactive] public int PostalCode { get; set; }
+        [Reactive] public string City { get; set; }
+        [Reactive] public string Country { get; set; }
+        [Reactive] public bool IsPickingLocationFromMap { get; set; }
+        public IReactiveCommand HandleOpenMap { get; set; } 
+    }
+}
